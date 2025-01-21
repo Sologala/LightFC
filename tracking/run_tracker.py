c01a4188e207f71881115b62342d552ed791d79d
WenDawson <dawsonwen123@gmail.com>
2025-01-22 00:46:43 +0800

    add run_tracker script

--- tracking/run_tracker.py
+++ tracking/run_tracker.py
@@ -15,11 +15,11 @@ from lib.test.evaluation.tracker import Tracker
 from lib.utils.load import load_yaml
 
 import argparse
-parser =argparse.ArgumentParser()
+
+parser = argparse.ArgumentParser()
 parser.add_argument("video")
 args = parser.parse_args()
-tracker = Tracker(
-    name='lightfc', parameter_name="lightfc_datasets", dataset_name='OTB2015')
+tracker = Tracker(name="lightfc", parameter_name="lightfc_datasets", dataset_name="OTB2015")
 # video_path = '/home/wen/Downloads/1.2.mp4'
 # video_path = '/home/wen/Documents/1_1_20250114_大疆neo无人机-环绕画面_20250114_大疆neo无人机-环绕画面.mp4'
 video_path = args.video
@@ -33,42 +33,46 @@ params = TrackerParams()
 # params.search_size = 256
 # params.checkpoint = '/home/devon/Project/TRACKER/SOT/LightFC/output/checkpoints/train/lightfc/mobilnetv2_p_pwcorr_se_scf_sc_iab_sc_adj_concat_repn33_se_conv33_center_wiou/lightfc_ep0400.pth.tar'
 
-yaml_file = '/home/wen/LightFC/experiments/lightfc/mobilnetv2_p_pwcorr_se_scf_sc_iab_sc_adj_concat_repn33_se_conv33_center_wiou.yaml'
+yaml_file = "experiments/lightfc/mobilnetv2_p_pwcorr_se_scf_sc_iab_sc_adj_concat_repn33_se_conv33_center_wiou.yaml"
 params.cfg = load_yaml(yaml_file)
 params.template_factor = 2.0
 params.template_size = 128
-params.search_factor =  4.0
+params.search_factor = 4.0
 params.search_size = 256
-params.checkpoint = '/home/wen/LightFC/output/checkpoints/train/lightfc/mobilnetv2_p_pwcorr_se_scf_sc_iab_sc_adj_concat_repn33_se_conv33_center_wiou/lightfc_ep0400.pth.tar'
+params.checkpoint = "./models/official/lightfc/checkpoints/train/lightfc/mobilnetv2_p_pwcorr_se_scf_sc_iab_sc_adj_concat_repn33_se_conv33_center_wiou/lightfc_ep0400.pth.tar"
 
 
-light_tracker =tracker.create_tracker(params)
+light_tracker = tracker.create_tracker(params)
+
 
 def _build_init_info(box):
-    return {'init_bbox': box}
+    return {"init_bbox": box}
+
 
 cap = cv.VideoCapture(video_path)
 frame_width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
 frame_height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
 frame_rate = int(cap.get(cv.CAP_PROP_FPS))
-print('frame_width:', frame_width)
-print('frame_height:', frame_height)
-print('frame_rate:', frame_rate)
-cap.set(cv.CAP_PROP_POS_FRAMES, 80 * 30)
+print("frame_width:", frame_width)
+print("frame_height:", frame_height)
+print("frame_rate:", frame_rate)
+cap.set(cv.CAP_PROP_POS_FRAMES, 0)
 
-writer = cv.VideoWriter('output_lightfc-vit.avi', cv.VideoWriter_fourcc(*'XVID'), frame_rate, (frame_width, frame_height))
+writer = cv.VideoWriter(
+    "output_lightfc-vit.avi", cv.VideoWriter_fourcc(*"XVID"), frame_rate, (frame_width, frame_height)
+)
 
 count = 20
 while count:
     ret, frame = cap.read()
-    count = count-1
-x, y, w, h  = cv.selectROI('Video', frame)
+    count = count - 1
+x, y, w, h = cv.selectROI("Video", frame)
 # x, y, w, h = 1619, 181, 52, 51
 
 init_state = [x, y, w, h]
 light_tracker.initialize(frame, _build_init_info(init_state))
 
-print('Start tracking...')
+print("Start tracking...")
 
 while True:
     ret, frame = cap.read()
@@ -80,24 +84,29 @@ while True:
 
     # Draw box
     out = light_tracker.track(frame)
-    state = [int(s) for s in out['target_bbox']]
+    state = [int(s) for s in out["target_bbox"]]
 
-    cv.rectangle(frame_disp, (state[0], state[1]), (state[2] + state[0], state[3] + state[1]),
-                     (0, 255, 0), 2)
+    cv.rectangle(frame_disp, (state[0], state[1]), (state[2] + state[0], state[3] + state[1]), (0, 255, 0), 2)
 
     font_color = (0, 0, 0)
-    cv.putText(frame_disp, 'Tracking!', (20, 30), cv.FONT_HERSHEY_COMPLEX_SMALL, 1,
-                font_color, 1)
-    cv.putText(frame_disp, 'Press r to reset', (20, 55), cv.FONT_HERSHEY_COMPLEX_SMALL, 1,
-                font_color, 1)
-    cv.putText(frame_disp, 'Press q to quit', (20, 80), cv.FONT_HERSHEY_COMPLEX_SMALL, 1,
-                font_color, 1)
-    
+    cv.putText(frame_disp, "Tracking!", (20, 30), cv.FONT_HERSHEY_COMPLEX_SMALL, 1, font_color, 1)
+    cv.putText(frame_disp, "Press r to reset", (20, 55), cv.FONT_HERSHEY_COMPLEX_SMALL, 1, font_color, 1)
+    cv.putText(frame_disp, "Press q to quit", (20, 80), cv.FONT_HERSHEY_COMPLEX_SMALL, 1, font_color, 1)
+
+    cv.putText(
+        frame_disp,
+        f'{out["score"]}',
+        list(map(int, (frame.shape[1] / 2, frame.shape[0] * 0.1))),
+        0,
+        1,
+        (255, 255, 0),
+        2,
+    )
+
     writer.write(frame_disp)
 
     # Display the resulting frame
-    cv.imshow('Video', frame_disp)
+    cv.imshow("Video", frame_disp)
     key = cv.waitKey(1)
-    if key == ord('q'):
+    if key == ord("q"):
         break
-
