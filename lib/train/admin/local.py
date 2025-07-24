5c0963a314557472284fc81a0115e078212e9399
WenDawson <dawsonwen123@gmail.com>
2025-07-24 23:49:04 +0800

    fix train

--- lib/train/admin/local.py
+++ lib/train/admin/local.py
@@ -1,12 +1,12 @@
 class EnvironmentSettings:
     def __init__(self, env_num=0):
-        self.workspace_dir = r'/home/liyunfeng/code/project2/LightFC'  # Base directory for saving network checkpoints.
-        self.tensorboard_dir = r'/home/liyunfeng/code/project2/LightFC/tensorboard'  # Directory for tensorboard files.
-        self.pretrained_networks = r'/home/liyunfeng/code/project2/LightFC/pretrained_models'
+        self.workspace_dir = r"/home/wen/ws/LightFC/output/"  # Base directory for saving network checkpoints.
+        self.tensorboard_dir = r"/home/wen/ws/LightFC/output/"  # Directory for tensorboard files.
+        self.pretrained_networks = r"/home/wen/ws/LightFC/pretraind_models/"
 
         self.lasot_dir = ''
-        self.got10k_dir = ''
-        self.got10k_val_dir = ''
+        self.got10k_dir = '/home/wen/ws/LightFC/data/got10k/train_data/'
+        self.got10k_val_dir = '/home/wen/ws/LightFC/data/got10k/val/'
         self.lasot_lmdb_dir = ''
         self.got10k_lmdb_dir = ''
         self.trackingnet_dir = ''
